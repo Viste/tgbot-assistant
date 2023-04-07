f86947fcf1cc7588eef66979b87c5312ed949909
viste <viste02@gmail.com>
2023-04-07 15:40:52 +0300

    update

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import sys
 
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.strategy import FSMStrategy
+from aiogram.fsm.storage.redis import RedisStorage
 
 from core import setup_routers
 from tools.utils import config
+from aioredis.client import Redis
 
+redis_client = Redis.from_url(config.redis_url)
 paper = Bot(token=config.token, parse_mode="HTML")
 
 
@@ -18,7 +21,8 @@ async def main():
         stream=sys.stdout,
     )
 
-    worker = Dispatcher(fsm_strategy=FSMStrategy.GLOBAL_USER)
+    storage = RedisStorage(redis=redis_client)
+    worker = Dispatcher(storage=storage, fsm_strategy=FSMStrategy.GLOBAL_USER)
     router = setup_routers()
     worker.include_router(router)
     useful_updates = worker.resolve_used_update_types()
