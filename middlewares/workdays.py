c3d547ca629bfb208ace69a921218665200a7065
viste <viste02@gmail.com>
2023-04-19 00:40:20 +0300

    update
    Signed-off-by: viste <viste02@gmail.com>

--- middlewares/workdays.py
+++ middlewares/workdays.py
@@ -1,33 +1,26 @@
 from datetime import datetime
-from typing import Callable, Dict, Any, Awaitable, Optional
+from typing import Callable, Dict, Any, Awaitable
 
 from aiogram import BaseMiddleware
 from aiogram.types import Message
+
 from core.admin import end_date
-
-
-def _is_working() -> bool:
-    now = datetime.now()
-    close_date = end_date[0]
-    if close_date is None:
-        return True
-    elif isinstance(close_date, datetime):
-        if now > close_date:
-            return True
-        else:
-            return False
-    else:
-        raise TypeError("end_date must be a datetime.datetime object or None")
 
 
 class WorkdaysMessageMiddleware(BaseMiddleware):
     async def __call__(
-        self,
-        handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
-        event: Message,
-        data: Dict[str, Any]
+            self,
+            handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+            event: Message,
+            data: Dict[str, Any]
     ) -> Any:
-        # Если не выключен и по датам все ок продолжаем
-        if not _is_working():
+        # If end_date is empty, treat it as if work is ongoing
+        if not end_date:
             return await handler(event, data)
-        # В противном случае просто вернётся None и обработка прекратится
+        # Otherwise, check the end date
+        close_date = end_date[0]
+        if close_date is None or isinstance(close_date, datetime) and datetime.now() > close_date:
+            return await handler(event, data)
+
+        # If the end date is in the future, stop processing
+        return None
