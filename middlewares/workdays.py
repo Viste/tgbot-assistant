0bc68e80f61383d5e2a687b452f02c5fa824cb4c
viste <viste02@gmail.com>
2023-04-19 14:23:22 +0300

    update midlle logic
    Signed-off-by: viste <viste02@gmail.com>

--- middlewares/workdays.py
+++ middlewares/workdays.py
@@ -17,9 +17,10 @@ session = AsyncSession()
 
 def _is_working() -> bool:
     now = datetime.now()
-    close_date = session.execute(select(Calendar).order_by(desc(Calendar.end_time)).limit(1))
+    result = session.execute(select(Calendar).order_by(desc(Calendar.end_time)).limit(1))
+    close_date = result.scalar_one()
 
-    if close_date is None or now > close_date:
+    if close_date.end_time is None or now > close_date.end_time:
         return True
     else:
         return False
@@ -32,7 +33,10 @@ class WorkdaysMessageMiddleware(BaseMiddleware):
             event: Message,
             data: Dict[str, Any]
     ) -> Any:
+        now = datetime.now()
+        result = await session.execute(select(Calendar).order_by(desc(Calendar.end_time)).limit(1))
+        close_date = result.scalar_one()
         # Если не выключен и по датам все ок продолжаем
-        if not _is_working():
+        if close_date.end_time is not None or now < close_date.end_time:
             return await handler(event, data)
         # В противном случае просто вернётся None и обработка прекратится
