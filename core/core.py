68fe95f065a4632c2fa55ea8d278b3324e4027a6
viste <viste02@gmail.com>
2023-04-23 17:19:50 +0300

    update
    Signed-off-by: viste <viste02@gmail.com>

--- core/core.py
+++ core/core.py
@@ -28,7 +28,7 @@ async def ask(message: types.Message, state: FSMContext) -> None:
         trimmed = trim_name(message.text)
 
         # Generate response
-        replay_text, total_tokens = await openai.get_response(uid, trimmed, chat_id)
+        replay_text, total_tokens = await openai.get_resp(trimmed, chat_id)
 
         chunks = split_into_chunks(replay_text)
         for index, chunk in enumerate(chunks):
@@ -57,7 +57,7 @@ async def process_ask(message: types.Message) -> None:
         trimmed = trim_name(message.text)
 
         # Generate response
-        replay_text, total_tokens = await openai.get_response(uid, trimmed, chat_id)
+        replay_text, total_tokens = await openai.get_resp(trimmed, chat_id)
         chunks = split_into_chunks(replay_text)
         for index, chunk in enumerate(chunks):
             try:
