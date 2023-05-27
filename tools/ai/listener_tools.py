792ad8a2677e8b39639deaa00ef890ebb1f00459
viste <viste02@gmail.com>
2023-05-27 04:32:44 +0300

    fix
    Signed-off-by: viste <viste02@gmail.com>

--- tools/ai/listener_tools.py
+++ tools/ai/listener_tools.py
@@ -12,7 +12,7 @@ logger = logging.getLogger("__name__")
 
 args = {
     "temperature": 0.1,
-    "max_tokens": 768,
+    "max_tokens": 256,
     "top_p": 1,
     "frequency_penalty": 0,
     "presence_penalty": 0.8,
