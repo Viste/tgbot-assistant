149843dc72f689382d72866e75586de88ef572a4
viste <viste02@gmail.com>
2023-06-09 05:07:35 +0300

    update:  some fix
    Signed-off-by: viste <viste02@gmail.com>

--- core/__init__.py
+++ core/__init__.py
@@ -5,8 +5,6 @@ from aiogram.filters.command import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import ReplyKeyboardRemove, Message
 
-from tools.utils import config
-
 logger = logging.getLogger("__name__")
 
 
@@ -16,7 +14,7 @@ def setup_routers() -> Router:
 
     router = Router()
 
-    @router.message(Command(commands=["cancel"]), F.chat.type.in_({'group', 'supergroup', 'private'}), F.chat.id.in_(config.allowed_groups))
+    @router.message(Command(commands=["cancel"]), F.chat.type.in_({'group', 'supergroup', 'private'}))
     @router.message(F.text.casefold() == "cancel")
     async def cancel_handler(message: Message, state: FSMContext) -> None:
         current_state = await state.get_state()
