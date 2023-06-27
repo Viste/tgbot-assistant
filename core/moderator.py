2e5ccaed1fd92e7323abaf1b088ec4e15c6f2d6b
viste <viste02@gmail.com>
2023-06-27 14:10:55 +0300

    update history in db
    Signed-off-by: viste <viste02@gmail.com>

--- core/moderator.py
+++ core/moderator.py
@@ -2,20 +2,22 @@ import html
 import logging
 
 from aiogram import types, F, Router, flags
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.helpers.tools import reply_if_banned
 from tools.ai.moderator import Moderator
-from tools.utils import split_into_chunks
+from tools.ai.user_dialogue import OpenAIDialogue
 
 logger = logging.getLogger(__name__)
 router = Router()
 router.message.filter(F.chat.type.in_({'group', 'supergroup'}), F.chat.id == -1001647523732)
+openai = OpenAIDialogue()
 moderator = Moderator()
 
 
 @flags.chat_action(action="typing", interval=1, initial_sleep=2)
 @router.message(F.text)
-async def process_moderating(message: types.Message) -> None:
+async def process_moderating(message: types.Message, session: AsyncSession) -> None:
     uid = message.from_user.id
     if await reply_if_banned(message, uid):
         return
@@ -23,8 +25,12 @@ async def process_moderating(message: types.Message) -> None:
         logging.info("%s", message)
         text = html.escape(message.text)
 
-        replay_text = await moderator.get_resp_mod(uid, text)
-        chunks = split_into_chunks(replay_text)
-        # for index, chunk in enumerate(chunks):
-        # if index == 0:
-        # await send_reply(message, chunk)
+        moderation = await moderator.query_gpt_mod(text)
+        print(moderation)
+        if moderation(moderation):
+            text = f'Пользователь академии @{uid}, использует  вынеси ему предупреждение'
+            # replay_text, total_tokens = await openai.get_resp(query=text, chat_id=uid, session=session)
+            # chunks = split_into_chunks(replay_text)
+            # for index, chunk in enumerate(chunks):
+            #    if index == 0:
+            #        await send_reply(message, chunk)
--- tools/ai/moderator.py
+++ tools/ai/moderator.py
@@ -13,22 +13,18 @@ class Moderator:
     def __init__(self):
         super().__init__()
         self.model = "text-moderation-latest"
-        self.max_tokens = 8192
         self.max_retries = 10
-        self.n_choices = 1
         self.retries = 0
-        self.args = {"temperature": 0, "max_tokens": 4096, "top_p": 1, "frequency_penalty": 0,
-                     "presence_penalty": 0.8, "stop": None}
 
-    async def _query_gpt_moderate(self, query):
-        while self.retries < self.max_retries:
+    async def query_gpt_mod(self, query):
+        for retry in range(self.max_retries):
             try:
                 response = await openai.Moderation.acreate(model=self.model, input=query)
-                result = response
+                result = response["results"][0]
                 logging.info("Result: %s", result)
-                break
-
+                return result
             except (openai.error.RateLimitError, openai.error.InvalidRequestError, Exception) as e:
+                self.retries += 1
                 if isinstance(e, openai.error.RateLimitError):
                     error_msg = f'⚠️OpenAI: Превышены лимиты ⚠️\n{str(e)}'
                 elif isinstance(e, openai.error.InvalidRequestError):
@@ -36,12 +32,6 @@ class Moderator:
                 else:
                     error_msg = f'⚠️Ошибочка вышла ⚠️\n{str(e)}'
                 result = {'choices': None, 'error': error_msg}
-            else:
-                break
-
-        return result
-
-    async def get_resp_mod(self, chat_id: int, query: str):
-        response = await self._query_gpt_moderate(query)
-
-        print(response["results"][0])
+                return result
+        logging.warning("Max retries exceeded for query: %s", query)
+        return {'choices': None, 'error': 'Max retries exceeded'}
