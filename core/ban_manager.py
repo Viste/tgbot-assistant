09856817ddbcdeb3a17707875a213122692c1b64
viste <viste02@gmail.com>
2023-07-02 23:17:03 +0300

    update

--- core/ban_manager.py
+++ core/ban_manager.py
@@ -6,7 +6,6 @@ from aiogram.filters import Command
 from fluent.runtime import FluentLocalization
 
 from core.helpers.tools import banned, shadowbanned, update_config
-from core.helpers.admin_manager import extract_id
 from tools.utils import config
 logger = logging.getLogger(__name__)
 
@@ -14,10 +13,23 @@ router = Router()
 router.message.filter(F.chat.type.in_({'private'}))
 
 
+def extract(message: types.Message) -> int:
+    entities = message.entities or message.caption_entities
+    logging.info("entities: %s", entities)
+    if not entities or entities[-1].type != "hashtag":
+        raise ValueError("Не удалось извлечь ID для ответа!")
+    hashtag = entities[-1].extract_from(message.text or message.caption)
+    logging.info("hashtag: %s", int(hashtag[3:]))
+    if len(hashtag) < 4 or not hashtag[3:].isdigit():
+        raise ValueError("Некорректный ID для ответа!")
+
+    return int(hashtag[3:])
+
+
 @router.message(Command(commands=["ban"]), F.reply_to_message, F.from_user.id.in_(config.admin_chat_id))
 async def cmd_ban(message: types.Message, l10n: FluentLocalization):
     try:
-        user_id = extract_id(message.reply_to_message)
+        user_id = extract(message.reply_to_message)
         logging.info("BAN EXTRACTED")
         banned.add(int(user_id))
         update_config()
@@ -29,7 +41,7 @@ async def cmd_ban(message: types.Message, l10n: FluentLocalization):
 @router.message(Command(commands=["shadowban"]), F.reply_to_message, F.from_user.id.in_(config.admin_chat_id))
 async def cmd_shadowban(message: types.Message, l10n: FluentLocalization):
     try:
-        user_id = extract_id(message.reply_to_message)
+        user_id = extract(message.reply_to_message)
         shadowbanned.add(int(user_id))
         update_config()
         await message.reply(l10n.format_value(msg_id="user-shadowbanned", args={"id": user_id}))
@@ -40,7 +52,7 @@ async def cmd_shadowban(message: types.Message, l10n: FluentLocalization):
 @router.message(Command(commands=["unban"]), F.reply_to_message, F.from_user.id.in_(config.admin_chat_id))
 async def cmd_unban(message: types.Message, l10n: FluentLocalization):
     try:
-        user_id = extract_id(message.reply_to_message)
+        user_id = extract(message.reply_to_message)
     except ValueError as ex:
         return await message.reply(str(ex))
     user_id = int(user_id)
