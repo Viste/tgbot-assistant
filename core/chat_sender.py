30c78b02ad71033672df11c373ab97ec2b048410
viste <viste02@gmail.com>
2024-01-08 21:07:24 +0300

    fix not needed html replace

--- core/chat_sender.py
+++ core/chat_sender.py
@@ -1,7 +1,7 @@
 import html
 import logging
 
-from aiogram import types, F, Router, flags
+from aiogram import types, F, Router, flags, Bot
 from fluent.runtime import FluentLocalization
 
 from core.helpers.tools import reply_if_banned
@@ -29,7 +29,7 @@ async def process_obs_text(message: types.Message, l10n: FluentLocalization) -> 
 
 @flags.chat_action(action="typing", interval=1, initial_sleep=2)
 @router.message(F.content_type.in_({'animation'}))
-async def process_obs(message: types.Message, l10n: FluentLocalization) -> None:
+async def process_obs(message: types.Message, l10n: FluentLocalization, bot: Bot) -> None:
     uid = message.from_user.id
     obs = ClientOBS()
     nickname = message.from_user.first_name + " " + (message.from_user.last_name if message.from_user.last_name else " ")
@@ -37,7 +37,6 @@ async def process_obs(message: types.Message, l10n: FluentLocalization) -> None:
         return
     else:
         logging.info("%s", message)
-        text = html.escape(message.text)
         file_id = message.animation.file_id
         file_info = await bot.get_file(file_id)
         file_url = f"https://api.telegram.org/file/bot{config.api_key}/{file_info.file_path}"
