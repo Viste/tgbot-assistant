290cda795a548057d69cd6424449b50809f24caf
viste <viste02@gmail.com>
2024-01-11 17:59:28 +0300

    update chat/forum senders and some

--- core/__init__.py
+++ core/__init__.py
@@ -9,7 +9,8 @@ logger = logging.getLogger(__name__)
 
 
 def setup_routers() -> Router:
-    from . import chat_dialogue, demo_catcher, email_catcher, demo_listener, chat_sender
+    from . import chat_dialogue, demo_catcher, email_catcher, demo_listener, forum_dialogue
+    from core.helpers import forum_sender
     from core.helpers import admin_manager, callbacks, payment, unsupported_manager
 
     router = Router()
@@ -29,7 +30,8 @@ def setup_routers() -> Router:
     router.include_router(admin_manager.router)
     router.include_router(unsupported_manager.router)
     router.include_router(chat_dialogue.router)
-    router.include_router(chat_sender.router)
+    router.include_router(forum_sender.router)
+    router.include_router(forum_dialogue.router)
     router.include_router(demo_catcher.router)
     router.include_router(email_catcher.router)
     router.include_router(demo_listener.router)
