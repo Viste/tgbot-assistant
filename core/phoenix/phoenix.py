ab658a4464ed2bdd494ac438d1d9610998bc6898
viste <viste02@gmail.com>
2024-02-20 23:07:12 +0300

    update with assistant feture

--- core/phoenix/phoenix.py
+++ core/phoenix/phoenix.py
@@ -7,7 +7,7 @@ from fluent.runtime import FluentLocalization
 from aiogram.filters import Command
 
 from core.helpers.tools import send_reply, reply_if_banned
-from tools.ai.ai_tools import OpenAI
+from tools.ai.assistant import OpenAIAssist
 from tools.states import Text
 from tools.utils import config, split_into_chunks
 
@@ -18,10 +18,10 @@ router.message.filter(F.chat.type.in_({'group', 'supergroup'}), F.chat.id.in_(co
 
 
 @flags.chat_action("typing")
-@router.message(F.text.regexp(r'[\s\S]+?\@cyberpaperbot[\s\S]+?'))
-async def ask(message: types.Message, state: FSMContext, l10n: FluentLocalization) -> None:
+@router.message(F.text.regexp('[\s\S]+?киберпапер[\s\S]+?'))
+async def assist(message: types.Message, state: FSMContext, l10n: FluentLocalization) -> None:
     await state.set_state(Text.get)
-    openai = OpenAI()
+    openai = OpenAIAssist()
 
     uid = message.from_user.id
     if await reply_if_banned(message, uid, l10n):
@@ -29,9 +29,9 @@ async def ask(message: types.Message, state: FSMContext, l10n: FluentLocalizatio
 
     logging.info("%s", message)
     text = html.escape(message.text)
-    escaped_text = text.strip('@cyberpaperbot ')
+    escaped_text = text.strip('киберпапер ')
 
-    replay_text, total_tokens = await openai.get_resp(escaped_text, uid)
+    replay_text, total_tokens = await openai.get_resp(escaped_text, uid, message.from_user.first_name)
     chunks = split_into_chunks(replay_text)
     for index, chunk in enumerate(chunks):
         if index == 0:
@@ -40,23 +40,18 @@ async def ask(message: types.Message, state: FSMContext, l10n: FluentLocalizatio
 
 @flags.chat_action("typing")
 @router.message(Text.get, F.reply_to_message.from_user.is_bot)
-async def process_ask(message: types.Message, l10n: FluentLocalization) -> None:
+async def process_assist(message: types.Message, l10n: FluentLocalization) -> None:
     uid = message.from_user.id
-    openai = OpenAI()
-    
+    openai = OpenAIAssist()
+
     if await reply_if_banned(message, uid, l10n):
         return
 
     logging.info("%s", message)
     text = html.escape(message.text)
 
-    replay_text, total_tokens = await openai.get_resp(text, uid)
+    replay_text, total_tokens = await openai.get_resp(text, uid, message.from_user.first_name)
     chunks = split_into_chunks(replay_text)
     for index, chunk in enumerate(chunks):
         if index == 0:
             await send_reply(message, chunk)
-
-
-@router.message(Command(commands="help"))
-async def info_user(message: types.Message, l10n: FluentLocalization):
-    await message.answer(l10n.format_value("help"))
---
+++ tools/ai/assistant.py
@@ -0,0 +1,79 @@
+import json
+import logging
+from calendar import monthrange
+from datetime import date
+
+from openai import AsyncOpenAI
+import requests
+import tiktoken
+from sqlalchemy import func, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.models import User
+from tools.utils import config
+
+logger = logging.getLogger(__name__)
+
+
+class OpenAIAssist:
+    max_retries: int
+
+    def __init__(self):
+        super().__init__()
+        self.model = "gpt-4-0125-preview"
+        self.client = AsyncOpenAI(api_key=config.api_key, base_url='http://176.222.52.92:9000/v1')
+        self.max_retries = 5
+        self.max_tokens = 128000
+        self.assistant_id = 'asst_XbFEiwIoTa196kOWZzi3zJQO'
+        self.config_tokens = 1024
+        self.max_history_size = 10
+        self.n_choices = 1
+        self.retries = 0
+        self.show_tokens = False
+
+    async def get_resp(self, query: str, chat_id: int, name: str) -> tuple[str, int]:
+        response = await self._query_gpt(chat_id, query, name)
+        answer = ''
+
+        logger.info('Response: %s, Answer: %s', response, answer)
+        if response.choices and len(response.choices) > 1 and self.n_choices > 1:
+            for index, choice in enumerate(response.choices):
+                content = choice.message.content.strip()
+                answer += f'{index + 1}\u20e3\n'
+                answer += content
+                answer += '\n\n'
+        elif response.choices and len(response.choices) >= 0:
+            answer = response.choices[0].message.content.strip()
+        else:
+            answer = response.choices[0].message.content.strip()
+
+        total_tokens = response.usage.total_tokens if response.usage else 0
+        if response.usage and self.show_tokens:
+            answer += "\n\n---\n" \
+                      f"💰 Использовано Токенов: {str(response.usage.total_tokens)}" \
+                      f" ({str(response.usage.prompt_tokens)} prompt," \
+                      f" {str(response.usage.completion_tokens)} completion)"
+
+        return answer, total_tokens
+
+    async def _query_gpt(self, user_id: int, query: str, name: str):
+        while self.retries < self.max_retries:
+            while self.retries < self.max_retries:
+                try:
+                    await self.client.beta.threads.messages.create(role="user", thread_id=str(user_id), content=query)
+                    await self.client.beta.threads.runs.create(thread_id=str(user_id), assistant_id=self.assistant_id, instructions=f"ник того с кем ты разговариваешь {name}")
+
+                    # Ждем пока Run перейдет в статус completed
+                    while True:
+                        run = await self.client.beta.threads.runs.retrieve(thread_id=str(user_id), run_id=run.id)
+                        logging.info('FROM NEW SPEAK WITH PAPER RUN: %s', run)
+                        if run.status == "completed":
+                            break
+
+                    messages = await self.client.beta.threads.messages.list(thread_id=str(user_id))
+                    logging.info('FROM NEW SPEAK WITH PAPER MESSAGE: %s', messages)
+                    return messages[-1]
+
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+                    self.retries += 1
