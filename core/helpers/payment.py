7e9b1052a878dee7fc73b74afdcf24db51822043
viste <viste02@gmail.com>
2024-02-26 07:15:04 +0300

    update payment.py for robokassa

--- core/helpers/payment.py
+++ core/helpers/payment.py
@@ -1,79 +1,47 @@
-import logging
-from datetime import datetime, timedelta
-
-from aiogram import types, Router, F, Bot
-from aiogram.fsm.context import FSMContext
-from aiogram.types import LabeledPrice
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from database.manager import UserManager as user_manager
-from database.models import User
-from tools.states import Payment
-from tools.utils import config
-
-router = Router()
-logger = logging.getLogger(__name__)
-
-price = [LabeledPrice(label='demo_room', amount=35000)]
+import aiohttp
+import decimal
+import hashlib
+from urllib import parse
+from urllib.parse import urlparse
 
 
-@router.message(Payment.process, F.content_type.in_({'text'}), F.chat.type == "private")
-async def pay_sub(message: types.Message, state: FSMContext, bot: Bot):
-    userid = message.from_user.id
-    current_state = await state.get_state()
-    logging.info("Current state: %r ", current_state)
-    await bot.send_invoice(userid, title='Приобретение подписки на сервис "Кибер Папер"',
-                           description='Приобрести Подписку', provider_token=config.payment_token, currency='RUB',
-                           photo_url='https://i.pinimg.com/originals/73/a1/ec/73a1ecc7f59840a47537c012bc23d685.png',
-                           photo_height=512, photo_width=512, photo_size=512, is_flexible=False, need_name=True,
-                           prices=price, start_parameter='create_invoice_subscribe', payload='payload:subscribe')
+def calculate_signature(*args) -> str:
+    """Create signature MD5."""
+    return hashlib.md5(':'.join(str(arg) for arg in args).encode()).hexdigest()
 
 
-@router.message(F.successful_payment)
-async def got_payment_ru(message: types.Message, state: FSMContext, session: AsyncSession):
-    current_state = await state.get_state()
-    logging.info("Current state: %r ", current_state)
+def parse_response(request: str) -> dict:
+    params = {}
 
-    logging.info('Info about message %s', message)
-    now = datetime.utcnow()
-    userid = message.from_user.id
-    user = await user_manager(session).get_user(userid)
-
-    if user is None:
-        user = User(telegram_id=message.from_user.id, telegram_username=message.from_user.username, balance_amount=350,
-                    max_tokens=0, current_tokens=0, subscription_start=now, subscription_end=now + timedelta(days=30),
-                    subscription_status='active', updated_at=now)
-        await user_manager(session).upsert_user(user)
-    else:
-        user.subscription_start = now
-        user.subscription_end = now + timedelta(days=30)
-        user.subscription_status = 'active'
-        user.telegram_id = message.from_user.id
-        user.telegram_username = message.from_user.username
-        user.balance_amount = 350
-
-    await session.commit()
-    await message.reply("Успех! Подписка оформлена")
-
-    if current_state is None:
-        return
-    logging.info("Cancelling state %r", current_state)
-    await state.clear()
+    for item in urlparse(request).query.split('&'):
+        key, value = item.split('=')
+        params[key] = value
+    return params
 
 
-@router.pre_checkout_query(lambda query: True)
-async def process_pay_sub(pre_checkout_query: types.PreCheckoutQuery, state: FSMContext, bot: Bot):
-    data = await state.get_data()
-    current_state = await state.get_state()
+def check_signature_result(order_number: int, received_sum: decimal.Decimal, received_signature: str, password: str) -> bool:
+    signature = calculate_signature(received_sum, order_number, password)
+    if signature.lower() == received_signature.lower():
+        return True
+    return False
 
-    if current_state is None:
-        return
-    elif "Payment" in current_state:
-        await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
-        await bot.send_message(chat_id=config.info_channel,
-                                 text=f"Пришла оплата за донат, ID платежа: {pre_checkout_query.id}\n"
-                                      f"Telegram user: {pre_checkout_query.from_user.first_name}\n"
-                                      f"Кто: {pre_checkout_query.order_info.name}")
 
-        logging.info("Current state %r, chat_id = %s", current_state, data)
-        await state.set_state(Payment.process)
+def generate_payment_link(merchant_login: str, merchant_password_1: str, cost: decimal.Decimal, number: int, description: str, is_test=0,
+                          robokassa_payment_url='https://auth.robokassa.ru/Merchant/Index.aspx') -> str:
+    """URL for redirection of the customer to the service."""
+    signature = calculate_signature(merchant_login, cost, number, merchant_password_1)
+
+    data = {'MerchantLogin': merchant_login, 'OutSum': cost, 'InvId': number, 'Description': description, 'SignatureValue': signature, 'IsTest': is_test}
+    return f'{robokassa_payment_url}?{parse.urlencode(data)}'
+
+
+async def verify_payment_async(verification_url: str) -> str:
+    """Asynchronously verify payment status."""
+    async with aiohttp.ClientSession() as session:
+        async with session.get(verification_url) as response:
+            if response.status == 200:
+                text = await response.text()
+                # Assuming the response text is the verification result
+                return text
+            else:
+                return "Verification failed"
