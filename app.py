cdb814d86d162fbfd5e586a406aa14d08e498986
viste <viste02@gmail.com>
2024-03-28 10:19:15 +0300

    update
    Signed-off-by: viste <viste02@gmail.com>

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ async def set_stream():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
